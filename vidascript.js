b66d4140051f155eadbd8a5ef04322a1cf3c16a6
yagojohnen-dot <yago.johnen@escola.pr.gov.br>
2026-04-06 08:25:01 -0300

    nustered

--- vidascript.js
+++
@@ -1,6 +0,0 @@
-if (enemy.health > 1) {
-    ctx.fillStyle = 'red';
-    ctx.fillRect(enemy.x - 20, enemy.y - enemy.radius - 10, 40, 5);
-    ctx.fillStyle = 'green';
-    ctx.fillRect(enemy.x - 20, enemy.y - enemy.radius - 10, (enemy.health / enemy.maxHealth) * 40, 5);
-}
